54f45a6725031896e2ca5adc6baf2e63cf7d7a29
Islam Solaiman <islam.solaiman92@gmail.com>
2024-02-13 20:27:36 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
+"""Route module for the API.
 """
 import os
 from os import getenv
-from flask_cors import (CORS, cross_origin)
 from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 
-from api.v1.auth.auth import Auth
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
